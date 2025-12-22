2df43be0da8f5adb047fc496941f13703b8c8499
Shubham-150-k <kumbhareshubham33@gmail.com>
2025-12-22 22:24:23 +0530

    Initial commit

---
+++ src/main/java/com/langchain/rag/chatbot/ChatBotConfig.java
@@ -0,0 +1,54 @@
+package com.langchain.rag.chatbot;
+
+import dev.langchain4j.chain.ConversationalRetrievalChain;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.cassandra.AstraDbEmbeddingConfiguration;
+import dev.langchain4j.store.embedding.cassandra.AstraDbEmbeddingStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatBotConfig {
+    @Bean
+    public EmbeddingModel embeddingModel() {
+        return new AllMiniLmL6V2EmbeddingModel();
+    }
+
+    @Bean
+    public AstraDbEmbeddingStore astraDbEmbeddingStore() {
+        String astraToken = "AstraCS:IKCAkpcnsofpnGifLDLgQLSk:54fc9ea6332d8e535d306b10a9c6752250358ef1b08d1d5668fd61603b9c35bb";
+        String databaseId = "23586df0-ae9d-4d35-8f29-9140993e35cb";
+
+        return new AstraDbEmbeddingStore(AstraDbEmbeddingConfiguration
+                .builder()
+                .token(astraToken)
+                .databaseId(databaseId)
+                .databaseRegion("us-east1")
+                .keyspace("pdfassistant")
+                .table("pdfchat")
+                .dimension(384)
+                .build());
+    }
+
+    @Bean
+    public EmbeddingStoreIngestor embeddingStoreIngestor() {
+        return EmbeddingStoreIngestor.builder()
+                .documentSplitter(DocumentSplitters.recursive(300, 0))
+                .embeddingModel(embeddingModel())
+                .embeddingStore(astraDbEmbeddingStore())
+                .build();
+    }
+
+    @Bean
+    public ConversationalRetrievalChain conversationalRetrievalChain() {
+        return ConversationalRetrievalChain.builder()
+                .chatLanguageModel(OpenAiChatModel.withApiKey("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"))
+                .retriever(EmbeddingStoreRetriever.from(astraDbEmbeddingStore(), embeddingModel()))
+                .build();
+    }
+}
